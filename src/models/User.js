02968d7c69b60eb06a87bb7e1d1985cb56b558aa
Snehahande05 <snehahande175@gmail.com>
2025-12-01 10:53:37 +0530

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const userSchema = {
+    name: {
+        type: String,
+        required: true,
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    role:{
+        type: String,
+        required: true,
+        enum: ["Librarian", "Student"],
+        default: "Student",
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+};
+
+const User = new mongoose.model("User",userSchema);
+module.exports = User;
